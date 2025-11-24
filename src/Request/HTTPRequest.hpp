9dda8f0517ad8ef2d6e8734295d6b4f0ba37021c
Luis Frade <luis.carvalho.frade@gmail..com>
2025-11-24 17:28:36 -0300

    updating to one class, keeping the idea of deep modules and information hiding

--- src/Request/HTTPRequest.hpp
+++ src/Request/HTTPRequest.hpp
@@ -1,39 +1,134 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HTTPRequest.hpp                                    :+:      :+:    :+:   */
+/*   testing_new_idea.hpp                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/04 09:58:08 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/11/17 18:02:32 by luide-ca         ###   ########.fr       */
+/*   Created: 2025/11/24 17:26:00 by luide-ca          #+#    #+#             */
+/*   Updated: 2025/11/24 17:26:02 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HTTP_REQUEST_HPP
 # define HTTP_REQUEST_HPP
 
-# include <iostream>
+# include <string>
+# include <map>
 # include <exception>
 
 class HTTPRequest {
-	private:
-			// receives full buffer and full length from recv call
-			std::string _buffer;
-			int _lengthBuffer;
+    private:
+        // raw input
+        std::string _buffer;
+        std::size_t _lengthBuffer;
 
-	public:
-			// orthodox-canonical form
-			HTTPRequest(void);
-			HTTPRequest(const std::string buffer, int lengthBuffer);
-			HTTPRequest(const HTTPRequest& other);
-			~HTTPRequest( /*feel with everything necessary*/ );
-			
-			HTTPRequest& operator=( const HTTPRequest& other );
+        // request line
+        std::string _method;
+        std::string _path;
+        std::string _httpVersion;
 
-			// specfic methods
-			void getBuffer(void) const;
-			int getLengthBuffer(void) const;
+        // headers
+        std::map<std::string, std::string> _headers;
+
+        // body
+        std::string _body;
+
+    public:
+        /*
+        ========== Orthodox Canonical Form
+        */
+        HTTPRequest();
+        HTTPRequest(const std::string &buffer, std::size_t lengthBuffer);
+        HTTPRequest(const HTTPRequest &other);
+        HTTPRequest &operator=(const HTTPRequest &other);
+        ~HTTPRequest();
+
+        /*
+        ========== Access to raw buffer info
+        */
+        const std::string &getFullBuffer() const;
+        std::size_t        getFullLengthBuffer() const;
+
+        /*
+        ========== Request line
+        */
+        const std::string &getMethod() const;
+        const std::string &getPath() const;
+        const std::string &getHTTPVersion() const;
+
+        void setMethod(const std::string &method);
+        void setPath(const std::string &path);
+        void setHTTPVersion(const std::string &version);
+
+        /*
+        ========== Headers
+        */
+        const std::map<std::string, std::string> &getHeaders() const;
+        std::string                               getHeader(const std::string &key) const;
+        void                                      addHeader(const std::string &key,
+                                                           const std::string &value);
+
+        /*
+        ========== Body
+        */
+        const std::string &getBody() const;
+        void               setBody(const std::string &body);
+
+        /*
+        ========== Parsing interface
+        */
+        void parseRequestLine(const std::string &buffer);
+        void parseHeaderLine(const std::string &buffer);
+        void parseBody(const std::string &buffer);
+
+        /*
+        ========== Exceptions
+        */
+        class InputException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        class MethodException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        class PathException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        class HTTPTypeException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        class RequestEndLineException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        class KeyHeaderException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        class ValueHeaderException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        class HeaderEndException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        class BodyException : public std::exception {
+        public:
+            virtual const char* what() const throw();
+        };
 };
 
 #endif
